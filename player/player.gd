24e40938cb6db6bc45e2ee83bd1790d1a2a81cfa
David <37672966+Ieida@users.noreply.github.com>
2023-12-18 19:26:41 +0100

    joystick looking and acceleration/deceleration

--- player/player.gd
+++ player/player.gd
@@ -1,12 +1,19 @@
 extends Node3D
 
 
-@export var speed: float = 5
-@export var sensitivity: float = 0.2
-var look_input: Vector2 = Vector2.ZERO
-@onready var camera := $CharacterBody3D/Camera3D
+@export_category("Movement")
 @onready var controller := $CharacterBody3D
-var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+@export var gravity:float = 9.8
+@export var base_speed: float = 5
+var speed: float = 0
+@export_range(0, 1) var acceleration: float = 0.5
+@export_range(0, 1) var deceleration: float = 0.5
+var input_vector: Vector3 = Vector3.ZERO
+@export_category("Looking")
+@onready var camera := $CharacterBody3D/Camera3D
+@export var sensitivity: float = 0.1
+@export var joystick_sensitivity: float = 0.1
+var look_input: Vector2 = Vector2.ZERO
 
 
 func _input(event):
@@ -23,29 +30,40 @@ func _process(delta):
 	if Input.is_action_just_pressed("ui_cancel"):
 		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
 		
-	look()
+	look(delta)
 
 
 func _physics_process(delta):
 	# Add gravity.
 	if not controller.is_on_floor():
 		controller.velocity.y -= gravity * delta
-		
-	move()
+	
+	move(delta)
 
 
-func look():
+func look(delta: float):
 	var horizontal = -look_input.x * sensitivity
+	horizontal += Input.get_axis("look_right", "look_left") * joystick_sensitivity * delta
 	var vertical = -look_input.y * sensitivity
+	vertical += Input.get_axis("look_up", "look_down") * joystick_sensitivity * delta
 	look_input = Vector2.ZERO
 	camera.rotate_y(deg_to_rad(horizontal))
 	camera.rotate_object_local(Vector3.RIGHT, deg_to_rad(vertical))
 
 
-func move():
+func move(delta: float):
 	var input_x = Input.get_axis("move_left", "move_right")
 	var input_z = Input.get_axis("move_forward", "move_backward")
-	var input_vector = (camera.global_basis.x * input_x + camera.global_basis.x.cross(controller.global_basis.y) * input_z).normalized()
+	var new_input_vector = (camera.global_basis.x * input_x + camera.global_basis.x.cross(controller.global_basis.y) * input_z).normalized()
+	if new_input_vector: input_vector = new_input_vector
+	
+	# Acceleration.
+	var target_speed = base_speed if new_input_vector else 0
+	var accel = acceleration if new_input_vector else deceleration
+	speed = move_toward(speed, target_speed, (base_speed * accel) * delta)
+	
+	# Deceleration.
+	
 	var movement = input_vector * speed
 	movement.y = controller.velocity.y
 	controller.velocity = movement
